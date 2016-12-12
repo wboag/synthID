df0828e5a2b94dfb873ba8b8a9f7761f166a02dc
mhkane <mhkane@mit.edu>
2016-12-12 01:18:27 -0500

    Binary with 1s and 0s

--- rnns/metrics.py
+++ rnns/metrics.py
@@ -10,6 +10,9 @@ def precision(reader, correct_tags,predicted_tags,binary=False, counts=False):
     false_positive = 0.0
     empty_tag = '<PAD>'
     outside_tag = 'OUTSIDE'
+    for cat in reader.tag_index:
+        per_tag_fp[cat]=0.0
+        per_tag_tp[cat]=0.0
 
     for sent in range(len(correct_tags)):
         correct_tag_list = reader.decode_tags(correct_tags[sent]).split(' ')
@@ -19,12 +22,14 @@ def precision(reader, correct_tags,predicted_tags,binary=False, counts=False):
             actual = correct_tag_list[tag]
             if actual == outside_tag and predicted !=outside_tag:
                 false_positive+=1.0
+                per_tag_fp[predicted]+=1.0
             if binary:
                 if actual != outside_tag and actual != empty_tag and predicted ==actual:
                     true_positive+=1.0
             else:
                 if actual != outside_tag and actual != empty_tag and predicted!= empty_tag and predicted != outside_tag:
                     true_positive+=1.0
+                    per_tag_tp[]
     total = (true_positive+false_positive)
     if counts:
         return true_positive, false_positive
@@ -64,13 +69,67 @@ def recall(reader, correct_tags,predicted_tags,binary=False, counts=False):
         return true_positive, false_negative
     return true_positive/total if total != 0 else float('nan')
 
+def precision_binary(reader, correct_tags,predicted_tags, counts=False):
+    '''Takes in a list of predictions, true tags as 0s and 1s and computes precision by defining:
+    False positive when actual = 0 and predicted a 1
+    True positive when actual = 1 and predicted = 1
+    returns : precision = (true_positive)/(true_positive+false_positive) score between 0.0 and 1.0  else nan if some error happened'''
+    true_positive = 0.0
+    false_positive = 0.0
+    for sent in range(len(correct_tags)):
+        correct_tag_list = reader.decode_tags(correct_tags[sent]).split(' ')
+        predicted_tag_list = reader.decode_tags(predicted_tags[sent]).split(' ')
+        for tag in range(len(correct_tag_list)):
+            predicted = predicted_tag_list[tag]
+            actual = correct_tag_list[tag]
+            if not(actual) and predicted:
+                false_positive+=1.0
+            if actual and predicted:
+                true_positive+=1.0
+    total = (true_positive+false_positive)
+    if counts:
+        return true_positive, false_positive
+    return true_positive/total if total != 0 else float('nan')
+def recall_binary(reader, correct_tags,predicted_tags, counts=False):
+    '''Takes in a list of predictions as 0s and 1s and computes recall by defining:
+    False positive when actual = 0 and predicted a 1
+    False Negative when actual = 1 and predicted = 0
+    True positive when actual = 1 and predicted = 1
+    Return recall = (true_positive/true_positive+false_negative)'''
+    true_positive = 0.0
+    false_negative = 0.0
+    false_positive = 0.0
+    #reader = data_wrapper.DataReader()
+
+    for sent in range(len(correct_tags)):
+        correct_tag_list = reader.decode_tags(correct_tags[sent]).split(' ')
+        predicted_tag_list = reader.decode_tags(predicted_tags[sent]).split(' ')
+        for tag in range(len(predicted_tag_list)):
+            predicted = predicted_tag_list[tag]
+            actual = correct_tag_list[tag]
+            if not(actual) and predicted:
+                false_positive+=1.0
+            elif actual and not(predicted):
+                false_negative+=1.0
+            elif actual and predicted:
+                true_positive+=1.0
+    total = (true_positive+false_negative)
+    if counts:
+        return true_positive, false_negative
+    return true_positive/total if total != 0 else float('nan')
+
 
 def f1(p,r):
+    '''Takes precision and recall as input and computes f1_score'''
     return (2.0*p*r)/(p+r) if (p + r) != 0 else float('nan')
 
 
 def precision_recall_f1(reader, correct_tags, predicted_tags, binary=False):
-    p = precision(reader, correct_tags, predicted_tags, binary=binary)
-    r = recall(reader, correct_tags, predicted_tags, binary=binary)
+    if not(binary):
+        p = precision(reader, correct_tags, predicted_tags, binary=False)
+        r = recall(reader, correct_tags, predicted_tags, binary=False)
+    else:
+        p = precision_binary(reader, correct_tags, predicted_tags)
+        r = recall_binary(reader, correct_tags, predicted_tags)
     f = f1(p, r)
     return p, r, f
---
+++ rnns/simple_bi_lstm_demo.py
@@ -0,0 +1,141 @@
+import tensorflow as tf
+import data_wrapper
+import metrics
+
+batch_size = 32
+hidden_units = 64
+learning_rate = .003
+training_steps = 10**5
+embedding_dim = 50  # 50, 100, 200, or 300
+
+graph = tf.Graph()
+session = tf.Session(graph=graph)
+reader = data_wrapper.DataReader(embedding_dim=embedding_dim, num_threads=3)
+output_units = len(reader.tag_index)
+print reader.tag_index
+
+def get_batch(batch_size, train=False):
+    load_batch = reader.get_train_batch if train else reader.get_test_batch
+    tokens, tags, lengths, filenames, line_nums = load_batch(batch_size)
+    embeddings = tf.get_variable(
+        name='embedding_matrix',
+        initializer=tf.constant_initializer(reader.embeddings),
+        shape=reader.embeddings.shape
+    )
+    inputs = tf.nn.embedding_lookup(embeddings, tokens)
+    return tokens, tags, lengths, inputs, filenames, line_nums
+
+
+def predict(inputs, lengths):
+    cell_forward = tf.nn.rnn_cell.BasicLSTMCell(hidden_units)
+    cell_backward = tf.nn.rnn_cell.BasicLSTMCell(hidden_units)
+
+    (a_fw,a_bw), _ = tf.nn.bidirectional_dynamic_rnn(cell_forward,cell_backward, inputs, lengths, dtype=tf.float32)
+    a = a_fw*a_bw
+
+    cell = tf.nn.rnn_cell.BasicLSTMCell(hidden_units)
+    num_layers = 2
+    cell_1 = tf.nn.rnn_cell.MultiRNNCell([tf.nn.rnn_cell.LSTMCell(hidden_units)]*num_layers)
+    a, _ = tf.nn.dynamic_rnn(cell_1, inputs, lengths, dtype=tf.float32)
+
+    W = tf.get_variable(
+        name='fc_weights',
+        initializer=tf.random_normal_initializer(),
+        shape=[hidden_units, output_units]
+    )
+    b = tf.get_variable(
+        name='fc_biases',
+        initializer=tf.constant_initializer(),
+        shape=[output_units]
+    )
+    z = tf.matmul(tf.reshape(a, [-1, hidden_units]), W) + b
+    z = tf.reshape(z, [-1, tf.shape(a)[1], output_units])
+    preds = tf.argmax(z, 2)
+    return z, preds
+
+
+def cross_entropy(logits, lengths, tags):
+    x_ent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, tags)
+    loss = tf.reduce_sum(x_ent) / tf.cast(tf.reduce_sum(lengths), tf.float32)
+    return loss
+
+
+def evaluate_test_set(session, tags, preds, fnames, lines, batch_limit=None):
+    batch_num = 0
+    num_sequences = 0
+    p_tp_total, p_fp_total, r_tp_total, r_fn_total = 0, 0, 0, 0
+
+    while True:
+        try:
+            y, y_, filenames, line_nums = \
+                session.run([tags, preds, fnames, lines])
+            p_tp, p_fp = metrics.precision(reader, y, y_, counts=True)
+            r_tp, r_fn = metrics.recall(reader, y, y_, counts=True)
+            p_tp_total += p_tp
+            p_fp_total += p_fp
+            r_tp_total += r_tp
+            r_fn_total += r_fn
+            num_sequences += len(y)
+            batch_num += 1
+            if batch_num == batch_limit:
+                break
+        except tf.errors.OutOfRangeError:
+            print 'test queue is empty'
+            break
+
+    if p_tp_total:
+        precision = p_tp_total / (p_tp_total + p_fp_total)
+        recall = r_tp_total / (r_tp_total + r_fn_total)
+        f1 = metrics.f1(precision, recall)
+
+        print 'Evaluated {} sequences from test set'.format(num_sequences)
+        print 'Precision:  ', precision
+        print 'Recall:     ', recall
+        print 'f1:         ', f1
+
+
+with graph.as_default():
+
+    with tf.variable_scope('rnn'):
+        tokens, tags, lengths, inputs, fnames, lines = \
+            get_batch(batch_size, train=True)
+        logits, preds = predict(inputs, lengths)
+        loss = cross_entropy(logits, lengths, tags)
+        step = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+    with tf.variable_scope('rnn', reuse=True):
+        test_tokens, test_tags, test_lengths, test_inputs, \
+            test_fnames, test_lines = get_batch(10000, train=False)
+        test_logits, test_preds = predict(test_inputs, test_lengths)
+        test_loss = cross_entropy(test_logits, test_lengths, test_tags)
+
+    init = tf.group(tf.global_variables_initializer(),
+                    tf.local_variables_initializer())
+
+
+with session.as_default():
+    session.run(init)
+    train_coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(session, train_coord)
+
+    for step_num in range(training_steps):
+        _, batch_loss, filenames, line_nums = \
+            session.run([step, loss, fnames, lines])
+        if step_num % 50 == 0:
+            x, y, y_ = session.run([tokens, tags, preds])
+            accuracy = 1.0 * ((y == y_) & (y != 0)).sum() / (y != 0).sum()
+            precision, recall, f1 = metrics.precision_recall_f1(reader, y, y_)
+
+            # print some info about the batch
+            print 'Loss:      ', batch_loss
+            print 'Precision: ', precision
+            print 'Recall:    ', recall
+            print 'f1:        ', f1
+            print 'Sentence:  ', reader.decode_tokens(x[0][(y != 0)[0]][:15])
+            print 'Truth:     ', reader.decode_tags(y[0][(y != 0)[0]][:15])
+            print 'Pred:      ', reader.decode_tags(y_[0][(y != 0)[0]][:15])
+            print
+
+    evaluate_test_set(session, test_tags, test_preds, test_fnames, test_lines)
+    train_coord.request_stop()
+    train_coord.join(threads)
